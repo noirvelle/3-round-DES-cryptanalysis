ce0313acbb330cc668f251589983fc72d0fa5b60
nataprawiraf <nataprawiraf@gmail.com>
2021-04-07 10:09:40 +0700

    checkpoint attack round 3

--- break_3_round_des.py
+++ break_3_round_des.py
@@ -2,29 +2,6 @@ from des import *
 from break_des import *
 
 def break_3_round_des():
-    print(f"""
-===========================
-    BREAK 3 ROUND DES
-===========================
-    """)
-    ptp1 = ("HQHQMAMA", "ZXWSMAMA")
-    ptp2 = ("AOMEZZSS", "QIELZZSS")
-    ptp3 = ("EXKIGHJK", "OPAWGHJK")
-
-    assert xor(string_to_bit_array(ptp1[0])[32:], string_to_bit_array(ptp1[1])[32:]) == [0] * 32
-    assert xor(string_to_bit_array(ptp2[0])[32:], string_to_bit_array(ptp2[1])[32:]) == [0] * 32
-    assert xor(string_to_bit_array(ptp3[0])[32:], string_to_bit_array(ptp3[1])[32:]) == [0] * 32
-
-    sk = "secret_k"
-
-    dp1 = (des(round=3), des(round=3))
-    dp2 = (des(round=3), des(round=3))
-    dp3 = (des(round=3), des(round=3))
-
-    ctp1 = (dp1[0].encrypt(key=sk, text=ptp1[0]), dp1[1].encrypt(key=sk, text=ptp1[1]))
-    ctp2 = (dp2[0].encrypt(key=sk, text=ptp2[0]), dp2[1].encrypt(key=sk, text=ptp2[1]))
-    ctp3 = (dp3[0].encrypt(key=sk, text=ptp3[0]), dp3[1].encrypt(key=sk, text=ptp3[1]))
-
     def get_possible_k3(des, ciphertext):
 
         # L0: left half of plaintext, R0: right half of plaintext
@@ -36,8 +13,6 @@ def break_3_round_des():
 
         # because R0 == R0*
         # f(R0, k1) + f(R0*, k1) == 0
-
-        # because we know L2 and R2
 
         # f(L3, k3)' 
         # = (R3 + L2) + (R3 + L2)* 
@@ -51,7 +26,7 @@ def break_3_round_des():
         # R3' = [L0 + f(R0, K1) + f(L3, K3)] + [L0* + f(R0, K1)* + f(L3, K3)*]
         # R3' = L0' + f(R0,K1)' + f(L3,K3)' 
         # -> f(R0,K1)' = 0
-        # maka
+        # then
         # R3' = L0' + f(L3,K3)' 
         # f(L3, K3)' = R3' + L0'
         R3_ = xor_string(R3_1, R3_2)
@@ -72,66 +47,87 @@ def break_3_round_des():
 
         E_R2_ = nsplit(E_R2_, 6)
 
-        return get_possible_sbox_input(E_R2_, F_L3_K3_)
+        return get_possible_subkeys_from_sbox(E_R2_, F_L3_K3_)
 
-    possible_k_pt1 = get_possible_k3(dp1, ctp1)
-    possible_k_pt2 = get_possible_k3(dp2, ctp2)
-    possible_k_pt3 = get_possible_k3(dp3, ctp3)
+    print(f"""
+===========================
+    BREAK 3 ROUND DES
+===========================
+    """)
+    ptp1 = ("HQHQMAMA", "ZXWSMAMA")
+    ptp2 = ("AOMEZZSS", "QIELZZSS")
+    ptp3 = ("EXKIGHJK", "OPAWGHJK")
 
-    from pprint import pprint
-    pprint(possible_k_pt1[0])
-    pprint(possible_k_pt2[0])
-    pprint(possible_k_pt3[0])
-    
+    sk = "secret_k"
+
+    dp1 = (des(round=3), des(round=3))
+    dp2 = (des(round=3), des(round=3))
+    dp3 = (des(round=3), des(round=3))
+
+    ctp1 = (dp1[0].encrypt(key=sk, text=ptp1[0]), dp1[1].encrypt(key=sk, text=ptp1[1]))
+    ctp2 = (dp2[0].encrypt(key=sk, text=ptp2[0]), dp2[1].encrypt(key=sk, text=ptp2[1]))
+    ctp3 = (dp3[0].encrypt(key=sk, text=ptp3[0]), dp3[1].encrypt(key=sk, text=ptp3[1]))
+
+    possible_subkeys = []
+
+    possible_subkeys.append(get_possible_k3(dp1, ctp1))
+    possible_subkeys.append(get_possible_k3(dp2, ctp2))
+    possible_subkeys.append(get_possible_k3(dp3, ctp3))
+
+    get_intersect_keys = get_intersect_key_dict_diff(possible_subkeys[0], possible_subkeys[1], possible_subkeys[2])
+
+    print(get_intersect_keys)
+
     K3 = get_intersect_key_diff(possible_k_pt1, possible_k_pt2, possible_k_pt3)
+    actual_K3 = key_to_subkeys(dp1[0].keys[3], 6)
 
     print(f"""found K1: \t\t{K3}""")
-    print(f"""actual K1: \t\t{dp1[0].keys[0]}""")
+    print(f"""actual K1: \t\t{actual_K3}""")
 
-    assert K3 == dp1[0].keys[2]
+    # assert K3 == actual_K3
 
-    def get_possible_k2(des, ciphertext):
+    # def get_possible_k2(des, ciphertext):
 
-        # L0: left half of plaintext, R0: right half of plaintext
-        L0, R0 = divide_half(des.text)
+    #     # L0: left half of plaintext, R0: right half of plaintext
+    #     L0, R0 = divide_half(des.text)
 
-        # L1 = R0
-        L1 = R0
+    #     # L1 = R0
+    #     L1 = R0
 
-        # L1: left half of ciphertext, R1: right half of ciphertext
-        R2, L2 = divide_half(ciphertext)
+    #     # L1: left half of ciphertext, R1: right half of ciphertext
+    #     R2, L2 = divide_half(ciphertext)
 
-        # R1 = L2
-        R1 = L2
+    #     # R1 = L2
+    #     R1 = L2
 
-        # expansion box input (E is Expansion table)
-        E_R1 = des.expand(string_to_bit_array(R1), E)
+    #     # expansion box input (E is Expansion table)
+    #     E_R1 = des.expand(string_to_bit_array(R1), E)
 
-        assert E_R1 == des.data["E1"]
+    #     assert E_R1 == des.data["E1"]
 
-        E_R1 = nsplit(E_R1, 6)
+    #     E_R1 = nsplit(E_R1, 6)
 
-        # L2 = R1 = L0 + f (R0, k 1)
-        # L2 = L0 + f(R0, k1)
-        # f(R0, k1) =  L2 + L0
+    #     # L2 = R1 = L0 + f (R0, k 1)
+    #     # L2 = L0 + f(R0, k1)
+    #     # f(R0, k1) =  L2 + L0
 
-        # F(R1, K2) = R2 + L1
-        F_R1_K2 = des.xor(string_to_bit_array(R2), string_to_bit_array(L1))
+    #     # F(R1, K2) = R2 + L1
+    #     F_R1_K2 = des.xor(string_to_bit_array(R2), string_to_bit_array(L1))
 
-        # F_R0_K1
-        assert F_R1_K2 == des.data["F1"]
+    #     # F_R0_K1
+    #     assert F_R1_K2 == des.data["F1"]
 
-        F_R1_K2 = nsplit(F_R1_K2, 4)
+    #     F_R1_K2 = nsplit(F_R1_K2, 4)
 
-        return get_possible_sbox_input(E_R1, F_R1_K2)
+    #     return get_possible_sbox_input(E_R1, F_R1_K2)
 
-    possible_k_pt1 = get_possible_k2(d1, ct1)
-    possible_k_pt2 = get_possible_k2(d2, ct2)
-    possible_k_pt3 = get_possible_k2(d3, ct3)
+    # possible_k_pt1 = get_possible_k2(d1, ct1)
+    # possible_k_pt2 = get_possible_k2(d2, ct2)
+    # possible_k_pt3 = get_possible_k2(d3, ct3)
 
-    K2 = get_intersect_key(possible_k_pt1, possible_k_pt2, possible_k_pt3)
+    # K2 = get_intersect_key(possible_k_pt1, possible_k_pt2, possible_k_pt3)
 
-    print(f"""found K2: \t\t{K2}""")
-    print(f"""actual K2: \t\t{d1.keys[1]}""")
+    # print(f"""found K2: \t\t{K2}""")
+    # print(f"""actual K2: \t\t{d1.keys[2]}""")
 
-    assert K2  == d1.keys[1]
+    # assert K2  == d1.keys[1]
--- break_des.py
+++ break_des.py
@@ -1,9 +1,11 @@
 from itertools import product
 from des import *
 
-def get_list_possible_sbox_input(sbox_index, expand_result, f_box_result):
-    output = bit_list_to_integer(f_box_result)
-    possible_input = []
+import random
+
+def get_possible_sbox_inputs(sbox_index, expand_result, sbox_ouput):
+    output = bit_list_to_integer(sbox_ouput)
+    possible_sbox_input = []
     for row in range(len(S_BOX[sbox_index])):
         for column in range(len(S_BOX[sbox_index][row])):
             if S_BOX[sbox_index][row][column] == output:
@@ -15,9 +17,9 @@ def get_list_possible_sbox_input(sbox_index, expand_result, f_box_result):
                 while len(coll) < 4:
                     coll = [0] + coll
 
-                possible_input.append([roww[0], coll[0], coll[1], coll[2], coll[3], roww[1]])
+                possible_sbox_input.append([roww[0], coll[0], coll[1], coll[2], coll[3], roww[1]])
+    return possible_sbox_input
 
-    return possible_input
 
 def right_shift(bit_list, n):
     from collections import deque
@@ -25,19 +27,36 @@ def right_shift(bit_list, n):
     bit_list_shifted.rotate(n)
     return list(bit_list_shifted)
 
+
 def get_possible_sbox_input(E_R, F_R_K):
     possible_k_list = []
     for i in range(len(E_R)):
         # getting possible row column value to construct S_Box input
         # getting row0,col0,col1,col2,col3,row1
         # there are four possible inputs SBOX
-        possible_row_col = get_list_possible_sbox_input(i, E_R[i], F_R_K[i])
+        possible_row_col = get_possible_sbox_inputs(i, E_R[i], F_R_K[i])
 
         # row0,col0,col1,col2,col3,row1 = E(R0) + K1
         # K1 = row0,col0,col1,col2,col3,row1 + E(R0)  
         # this possible key just contains of 6 bit of total 48 key bit
         possible_k_list.append([ xor(E_R[i], possible_input) for possible_input in possible_row_col ])
     return possible_k_list
+
+
+def get_possible_subkeys_from_sbox(sbox_input, sbox_output):
+    possible_k_list = {}
+    for i in range(len(sbox_input)):
+        # getting possible row column value to construct S_Box input
+        # getting row0,col0,col1,col2,col3,row1
+        # there are four possible inputs SBOX
+        possible_row_col = get_possible_sbox_inputs(i, sbox_input[i], sbox_output[i])
+
+        # row0,col0,col1,col2,col3,row1 = E(R0) + K1
+        # K1 = row0,col0,col1,col2,col3,row1 + E(R0)  
+        # this possible key just contains of 6 bit of total 48 key bit
+        possible_k_list[f"K{i+1}"] = [ xor(sbox_input[i], possible_input) for possible_input in possible_row_col ]
+    return possible_k_list
+
 
 def get_intersect_key(possible_k_pt1, possible_k_pt2, possible_k_pt3):
     retrieved_key = []
@@ -48,6 +67,7 @@ def get_intersect_key(possible_k_pt1, possible_k_pt2, possible_k_pt3):
                 break
 
     return retrieved_key
+
 
 def get_intersect_key_diff(possible_k_pt1, possible_k_pt2, possible_k_pt3):
     retrieved_key = {}
@@ -63,3 +83,62 @@ def get_intersect_key_diff(possible_k_pt1, possible_k_pt2, possible_k_pt3):
     return retrieved_key
 
 
+def get_intersect_key_dict_diff(possible_k_pt1, possible_k_pt2, possible_k_pt3):
+    retrieved_key = {}
+    for s in range(8):
+        for i,j,k in product(list(range(4)), list(range(4)), list(range(4))):
+            if possible_k_pt1[f"K{0+1}"][i] == possible_k_pt2[f"K{0+1}"][j] or possible_k_pt2[f"K{0+1}"][j] == possible_k_pt3[f"K{0+1}"][k] or possible_k_pt1[f"K{0+1}"][i] == possible_k_pt3[f"K{0+1}"][k]:
+                retrieved_key[f"K{0+1}"] = possible_k_pt1[f"K{0+1}"][i]
+                break
+
+        # if len(retrieved_key) != (s+1) * 6:
+        #     retrieved_key += [-1] * 6
+
+    return retrieved_key
+
+
+def key_to_subkeys(keys, n):
+    keys_splitted = nsplit(keys, n)
+    subkeys = {}
+    for subkey in range(len(keys_splitted)):
+        subkeys[f"K{subkey}"] = keys_splitted[subkey]
+    return subkeys
+
+
+# def get_intersect_keys(list_of_possible_keys):
+#     retrieved_key = {}
+#     for s in range(8):
+#         for i,j,k in product(list(range(4)), list(range(4)), list(range(4))):
+#             if possible_k_pt1[s][i] == possible_k_pt2[s][j] or possible_k_pt2[s][j] == possible_k_pt3[s][k] or possible_k_pt1[s][i] == possible_k_pt3[s][k]:
+#                 retrieved_key[f"K{s}"] = possible_k_pt1[s][i]
+#                 break
+
+#         # if len(retrieved_key) != (s+1) * 6:
+#         #     retrieved_key += [-1] * 6
+
+#     return retrieved_kes
+
+
+def arrange_key(list_of_possible_subkeys):
+    possible_keys = []
+    for possible_subkey in list_of_possible_subkeys:
+        # possible_subkey {K1: [a,b..], [c,d..], K2: []}
+        pass
+
+class PlaintextRandomGenerator:
+    def generate(self, difference=None):
+        # TODO validate difference
+
+        plaintext = random.randint(0, 2 ** 64 - 1)
+        plaintext = "{:0>16x}".format(plaintext)
+        plaintext = bytearray.fromhex(plaintext)
+
+        if difference == None:
+            return plaintext
+
+        plaintext = (
+            plaintext,
+            bytearray(p ^ d for p, d in zip(plaintext, difference))
+        )
+
+        return plaintext
