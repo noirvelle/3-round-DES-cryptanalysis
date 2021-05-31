bf004ebabe34886313b9fdee47b30db16505b1d6
nataprawiraf <nataprawiraf@gmail.com>
2021-05-31 17:55:52 +0700

    remove comment

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from break_1_round_des import break_1_round_des
 from break_2_round_des import break_2_round_des
 from break_3_round_des import break_3_round_des
 
-# break_1_round_des()
-# break_2_round_des()
+break_1_round_des()
+break_2_round_des()
 break_3_round_des()
